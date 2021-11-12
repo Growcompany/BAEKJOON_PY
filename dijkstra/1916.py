e8e43eb70e2364ba69d5459b0c8e4b9511948913
Growcompany <jack834@naver.com>
2021-11-12 13:26:15 +0000

    .

---
+++ dijkstra/1916.py
@@ -0,0 +1,38 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+
+INF = 1e9
+
+graph = [[] for _ in range(N+1)]
+distance = [INF]*(N+1)
+
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b,c))
+
+def dijkstra(start,end):
+    q = []
+    distance[start] = 0
+    
+    heapq.heappush(q,(0,start))
+    
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist+i[1]
+            if cost< distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q,(cost, i[0]))
+            
+        
+    return distance[end]
+
+start, end = map(int, input().split())
+print(dijkstra(start, end))
