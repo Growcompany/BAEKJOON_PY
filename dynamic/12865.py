c017b8e59630d6816c176d8bbf5d09913fda4af2
Growcompany <jack834@naver.com>
2021-11-13 04:39:46 +0000

    .

---
+++ dynamic/12865.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+item = [(0,0)]
+
+for _ in range(N):
+    W, V = map(int, input().split())
+    item.append((W,V))
+
+dp = [[0]*(K+1) for _ in range(N+1)]
+
+for i in range(1,N+1):
+    for j in range(1,K+1):
+        if j >= item[i][0]:
+            dp[i][j] = max(item[i][1]+dp[i-1][j-item[i][0]],dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[-1][-1])
