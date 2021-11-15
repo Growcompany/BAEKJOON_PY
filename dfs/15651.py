a359bce80b89b5479635f83b1ca215442d5fd7a4
Growcompany <jack834@naver.com>
2021-11-15 02:25:52 +0000

    .

---
+++ dfs/15651.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+
+def print_number():
+    for i in range(1,N):
+        
+
+for i in range(1,N):
+    for j in range(1,N):
+        print(i, end = ' ')
+        print(j)
+
+for i in range(1,N):
+    for j in range(1,N):
+        for k in range(1,N):
+            print(i, end = ' ')
+            print(j, end = ' ')
+            print(k)
+    
+        
