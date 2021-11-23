2435e4cdfd69c7354aacf83e1b7df9cf18c591fe
Growcompany <jack834@naver.com>
2021-11-23 10:16:33 +0000

    .

---
+++ stackqueue/heapq/1927.py
@@ -0,0 +1,21 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+q = []
+
+for _ in range(N):
+    num = int(input().rstrip('\n'))
+    if num == 0:
+        if len(q) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(q))
+    else:
+        heapq.heappush(q,num)
+        
+        
+    
