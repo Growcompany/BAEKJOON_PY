47a0fa9127ce0abf09c658b01515844faa56c475
Growcompany <jack834@naver.com>
2021-11-30 13:53:11 +0000

    .

---
+++ dynamic/14226.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+S = int(input())
+
+imoticon = [[-1]*(S+1) for _ in range(S+1)]
+
+imoticon[1][0] = 0
+q = deque()
+q.append((1,0))
+
+while q:
+    x, y = q.popleft() #xx는 좌표 yy는 클립보드에 저장된 숫자
+    if imoticon[x][x] == -1:
+        imoticon[x][x] = imoticon[x][y]+1
+        q.append((x,x))
+    if x+y<=S and imoticon[x+y][y] == -1:
+        imoticon[x+y][y] = imoticon[x][y]+1
+        q.append((x+y,y))
+    if 0<= x-1 and imoticon[x-1][y] == -1:
+        imoticon[x-1][y] = imoticon[x][y]+1
+        q.append((x-1,y))
+
+result =0
+for i in range(S):
+    if imoticon[S][i] >= 1:
+        result = min(imoticon[S][i],result)
+print(result)
