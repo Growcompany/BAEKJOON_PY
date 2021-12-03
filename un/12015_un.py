17051e8140aeb24052602b2bca192da20204f664
Growcompany <jack834@naver.com>
2021-12-03 23:55:16 +0000

    .

--- un/12015_un.py
+++ un/12015_un.py
@@ -1,22 +1,21 @@
+from bisect import bisect_left
+import sys
 
+input = sys.stdin.readline
 
+N = int(input())
+A_list = list(map(int,input().split()))
 
+save_list = []
+save_list.append(A_list[0])
 
-
-
-
-
-#답
-from bisect import bisect_left #이진탐색 코드, 같은 수일 경우 왼쪽 index를 돌려준다
-
-input()
-A = list(map(int, input().split()))
-dp = []
-
-for i in A:
-    k = bisect_left(dp, i) #자신이 들어갈 위치 k
-    if len(dp) <= k: #i가 가장 큰 숫자라면
-        dp.append(i)
+for i in range(1,N):
+    pos = bisect_left(save_list,A_list[i])
+    if len(save_list) <= pos:
+        save_list.append(A_list[i])
     else:
-        dp[k] = i #자신보다 큰 수 중 최솟값과 대체
-print(len(dp))
+        save_list[pos] = A_list[i]
+
+print(len(save_list))
+        
+#정확한 수열을 구해내지는 못하지만 이분탐색을 활용하여 빠른속도로 길이만 알아내는 대에는 유용하다.
