0274071173b6240ef530b11d31389d53bafddd5c
Growcompany <jack834@naver.com>
2021-12-04 14:23:49 +0000

    .

--- stackqueue/heapq/2075.py
+++ stackqueue/heapq/2075.py
@@ -4,11 +4,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-q = [0,0,0,0,0]
+q = []
 
 for _ in range(N):
     line = list(map(int,input().rstrip('\n').split()))
     for num in line:
+        if len(q) < N:
+            heapq.heappush(q,num)
         if q[0] < num:
             heapq.heappop(q)
             heapq.heappush(q,num)
