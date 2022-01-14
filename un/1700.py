9349a67b1d5f42019ac87fb7eb86958bead10088
Growcompany <jack834@naver.com>
2022-01-14 14:58:57 +0000

    .

---
+++ un/1700.py
@@ -0,0 +1,48 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int,input().split())
+order = list(map(int,input().split()))
+
+result = 0
+
+if N<K:
+    plug = order[:N]
+    for i in range(N,K):
+        if order[i] in plug:
+            print("같은거라 패스")
+            print(plug)
+            continue
+        
+        del_num_index = -1
+        farthest_index = -1
+
+        check = False
+        for j in range(N):
+            if not plug[j] in order[i:]:
+                print("뒤에 없는 가장 하빠리라 제거, 제거숫자:",plug[j])
+                plug[j] = order[i]
+                print(plug)
+                result +=1
+                check = True
+                break
+            else:
+                if farthest_index<order[i:].index(plug[j]):
+                    farthest_index =order[i:].index(plug[j])
+                    del_num_index = j
+        
+        if check:
+            continue
+        else:
+            print("뒤에 있는 거중에 가장 멀리 있는거 제일 멀리 측정된 인덱스:",del_num_index,"제거숫자:",plug[del_num_index])
+            plug[del_num_index] = order[i]
+            print(plug)
+            result +=1
+                    
+print(result)
+
+#반례
+#3 11
+#11 8 11 7 2 8 2 7 5 10 2
+    
