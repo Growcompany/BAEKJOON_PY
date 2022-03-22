37c1b90828aad42bb3a990adc31a949b7af1691a
Growcompany <jack834@naver.com>
2022-03-22 13:55:08 +0000

    .

--- stackqueue/1939.py
+++ stackqueue/1939.py
@@ -1,35 +1,44 @@
 import sys
-import copy
 from collections import deque
-
-sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
 N,M = map(int,input().split())
-
 graph = [[] for _ in range(N+1)]
 
 for _ in range(M):
-    start , end, weight = map(int,input().split())
-    graph[start].append((end,weight))
-    graph[end].append((start,weight))
+    a, b, weight = map(int,input().split())
+    graph[a].append((b,weight))
+    graph[b].append((a,weight))
 
-start,end = map(int,input().split())
+start, end = map(int,input().split()) 
 
-def dfs(now,end,visited,min_weight):
-    global result
-    #print("now:",now,"end:",end,"visited:",visited,'min_weight:',min_weight)
-    if now == end:
-        result = max(result,min_weight)
-    else:
-        for n_node,n_weight in graph[now]:
-            if visited[n_node] == False:
+def bfs(mid):
+    q = deque()
+    q.append(start)
+    visited[start] = True
+    
+    while q:
+        now = q.pop()
+        
+        if now == end:
+            return True
+        
+        for n_node, weight in graph[now]:
+            if not visited[n_node] and mid>weight:
                 visited[n_node] = True
-                dfs(n_node,end,copy.deepcopy(visited),min(n_weight,min_weight))
-    
-result = 0      
-visited = [False]*(N+1)                    
-dfs(start,end,copy.deepcopy(visited),1e9)
-print(result)
-    
-    
+                q.append(n_node)
+        
+    return False
+
+
+min_v,max_v = 1, 1_000_000_000
+
+while min_v<=max_v:
+    visited = [False]*(N+1)
+    mid = (min_v+max_v)//2
+    if bfs(mid):
+        min_v = mid+1
+    else:
+        max_v = mid+1
+        
+print(min_v)
